934e0cb959827603292a68c81fba0cf2a2e4a1a4
chun_laptop <chunchen@redhat.com>
2014-11-04 22:13:58 +0800

    p

--- MJTravel/MJTravel/spiders/MJCtrip.py
+++ MJTravel/MJTravel/spiders/MJCtrip.py
@@ -4,6 +4,7 @@ import scrapy
 from MJTravel.items import MjctripItem
 from MJTravel.manufacture import ConfigMiaoJI
 from MJTravel.lib.common import remove_str
+from scrapy.selector import Selector
 import codecs
 
 mj_cf = ConfigMiaoJI("./spider_settings.cfg")
@@ -14,13 +15,17 @@ class MjctripSpider(scrapy.Spider):
     start_urls = mj_cf.get_starturls('ctrip_spider','start_urls')
 
     def parse(self, response):
+       travel_sel = Selector(response)
+#       link_browse = travel_sel.select("//a[@class='link_browse']/span/text()").extract()
        link = response.url
        title = response.xpath("//div[@class='ctd_head_left']/h2/text()").extract()
        if len(title) >= 1:  
          title = remove_str(title[0],'[\r\n\s]')
        else: 
          title = ''
-       b_count = response.xpath("//a[@class='link_browse']/span/text()").extract()
+#       b_count = response.xpath("//a[@class='link_browse']/span/text()").extract()
+       b_count= travel_sel.xpath("//a[@class='link_browse']/span/text()").extract()
+       print "++++++++++++", b_count
        if len(b_count) >= 1: 
          b_count = remove_str(b_count[0])
        else:
