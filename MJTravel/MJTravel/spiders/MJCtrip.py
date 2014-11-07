402c78afa9a05e9716fd75caf26a9eca2204cea0
chunchen <chunchen@redhat.com>
2014-11-07 14:33:16 +0800

    p

--- MJTravel/MJTravel/spiders/MJCtrip.py
+++ MJTravel/MJTravel/spiders/MJCtrip.py
@@ -1,35 +1,34 @@
 # -*- coding: utf-8 -*-
 
-import scrapy
 from MJTravel.items import MjctripItem
 from MJTravel.manufacture import ConfigMiaoJI
 from MJTravel.lib.common import remove_str
+
 from scrapy.selector import Selector
+from scrapy.contrib.spiders import CrawlSpider, Rule
 import codecs
+import scrapy
 
 mj_cf = ConfigMiaoJI("./spider_settings.cfg")
 
-class MjctripSpider(scrapy.Spider):
+class MjctripSpider(CrawlSpider):
     name = "MJCtrip"
     allowed_domains = ["www.ctrip.com"]
     start_urls = mj_cf.get_starturls('ctrip_spider','start_urls')
 
     def parse(self, response):
-       travel_sel = Selector(response)
-#       link_browse = travel_sel.select("//a[@class='link_browse']/span/text()").extract()
        link = response.url
        title = response.xpath("//div[@class='ctd_head_left']/h2/text()").extract()
        if len(title) >= 1:  
          title = remove_str(title[0],'[\r\n\s]')
        else: 
          title = ''
-#       b_count = response.xpath("//a[@class='link_browse']/span/text()").extract()
-       b_count= travel_sel.xpath("//a[@class='link_browse']/span/text()").extract()
+       b_count = response.xpath("//a[@class='link_browse']/span/text()").extract()
        print "++++++++++++", b_count
-       if len(b_count) >= 1: 
-         b_count = remove_str(b_count[0])
-       else:
-         b_count = '0'
+       #if len(b_count >= 1)
+       b_count = remove_str(b_count[0]) if len(b_count) >= 1 else '0'
+       #else:
+       #  b_count = '0'
        c_count = response.xpath("//a[@class='link_comment ']/span/text()").extract()
        if len(c_count) >= 1: 
          c_count = remove_str(c_count[0])
