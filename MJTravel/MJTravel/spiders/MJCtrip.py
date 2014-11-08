228398e1f0a8763ddcbbc82997d02fda1617ed01
chun_laptop <chunchen@redhat.com>
2014-11-08 10:38:31 +0800

    p

--- MJTravel/MJTravel/spiders/MJCtrip.py
+++ MJTravel/MJTravel/spiders/MJCtrip.py
@@ -6,6 +6,7 @@ from MJTravel.lib.common import remove_str
 
 from scrapy.selector import Selector
 from scrapy.contrib.spiders import CrawlSpider, Rule
+from scrapy.contrib.linkextractors.lxmlhtml import LxmlLinkExtractor
 import codecs
 import scrapy
 
@@ -13,35 +14,63 @@ mj_cf = ConfigMiaoJI("./spider_settings.cfg")
 
 class MjctripSpider(CrawlSpider):
     name = "MJCtrip"
-    allowed_domains = ["www.ctrip.com"]
+    allowed_domains = ["you.ctrip.com"]
     start_urls = mj_cf.get_starturls('ctrip_spider','start_urls')
+    print 'urls:::', start_urls
 
-    def parse(self, response):
+    rules = [
+             Rule(LxmlLinkExtractor(allow='you\.ctrip\.com/travels/haikou37/\w+', process_value='process_value'),
+             callback='parse_item',
+             follow=True),
+             Rule(LxmlLinkExtractor(allow='you\.ctrip\.com/travels/zhangjiajie23/\w+', process_value='process_value'),
+             callback='parse_item',
+             follow=True)
+            ]
+
+    def process_value(value):
+      m = re.search("javascript:;", value)
+      if m:
+        return m.group(1)
+
+    def parse_item(self, response):
+       item = MjctripItem()
+       
+       # 游记链接
        link = response.url
-       title = response.xpath("//div[@class='ctd_head_left']/h2/text()").extract()
-       if len(title) >= 1:  
-         title = remove_str(title[0],'[\r\n\s]')
-       else: 
-         title = ''
-       b_count = response.xpath("//a[@class='link_browse']/span/text()").extract()
-       print "++++++++++++", b_count
-       #if len(b_count >= 1)
-       b_count = remove_str(b_count[0]) if len(b_count) >= 1 else '0'
-       #else:
-       #  b_count = '0'
-       c_count = response.xpath("//a[@class='link_comment ']/span/text()").extract()
-       if len(c_count) >= 1: 
-         c_count = remove_str(c_count[0])
-       else:
-         c_count = '0'
 
+       # 游记标题
+       title = response.xpath("//div[@class='ctd_head_left']/h2/text()").extract()
+       title = remove_str(title[0],'[\r\n\s]') if len(title) >= 1 else ''
+
+       # 游记内容
        all_content = ""
        for content in response.xpath("//div[@class='ctd_main_body']//p/text()").extract():
          all_content += remove_str(remove_str(content,'\r'),'\s{2,}')
 
+       # 游记浏览数
+       b_count = response.xpath("//a[@class='link_browse']/span/text()").extract()
+       print "++++++++++++", b_count
+       b_count = remove_str(b_count[0]) if len(b_count) >= 1 else '0'
+
+       # 游记评论数
+       c_count = response.xpath("//a[@class='link_comment ']/span/text()").extract()
+       c_count = remove_str(c_count[0]) if len(c_count) >= 1 else '0'
+
+       # 游记评论
        all_comment = ""
        for comment in response.xpath("//p[@class='ctd_comments_text']/text()").extract():
          all_comment += remove_str(remove_str(comment),'\s{2,}')
 
-       return MjctripItem(travel_link=link, travel_title=title, travel_content=all_content, browse_count=b_count, comment_count=c_count, comment_content=all_comment)
+       item['travel_link'] = link
+       item['travel_title'] = title
+       item['travel_content'] = all_content
+       item['browse_count'] = b_count
+       item['comment_count'] = c_count
+       item['comment_content'] = all_comment
+
+       # 丢弃游记内容是空的
+       if item['travel_content'] == '':
+         return None
+
+       return item
 
