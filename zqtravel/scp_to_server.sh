369dfb1a3a773f47b96854e93521ee9f7708966d
chunchen <chunchen@localhost.localdomain>
2015-04-13 17:17:56 +0800

    p

--- zqtravel/scp_to_server.sh
+++ zqtravel/scp_to_server.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
 SOURCE='/home/chunchen/Music/miaoji/zqtravel'
-#SOURCE='/home/chunchen/Music/.miaoji/back/zqtravel'
-#DATA_SOURCE='/home/scrapy/data/mafengwo.bak20150319'
+DATA_SOURCE='/home/scrapy/data/mafengwo_scenicspot'
 DESTINATION='~/travel'
-USER='chunchen'
+USER='root'
 
 scp -r -p -P12301 $SOURCE $DATA_SOURCE $USER@121.41.20.59:$DESTINATION
+
+
+
+## 为tar加密/解密
+## 加密压缩
+#  tar czf - ${待压缩的目录或文件} | openssl des3 -salt -k ${明文密码}| dd of=${压缩后的文件名}
+## 解密解压
+#  dd if=${压缩文件} | openssl des3 -d | tar xzf -
