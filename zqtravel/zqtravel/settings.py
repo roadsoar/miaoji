0bc7eee1186674dc5af1585fcf7d6f669126dbb1
roadsoar <chunchen@redhat.com>
2015-06-06 13:59:53 +0800

    p

--- zqtravel/zqtravel/settings.py
+++ zqtravel/zqtravel/settings.py
@@ -20,7 +20,6 @@ ITEM_PIPELINES = {
 # 'zqtravel.pipelines.ScenicspotPipeline': 801,
  'zqtravel.pipelines.ImagesStorePipeline': 801,
 # 'zqtravel.pipelines.TravelLinkPipeline': 803
-# 'scrapy.contrib.pipeline.images.ImagesPipeline': 1,
 }
 
 IMAGES_MIN_HEIGHT = 110
@@ -28,12 +27,6 @@ IMAGES_MIN_WIDTH = 110
 IMAGES_EXPIRES = 90   # 90天的图片失效期限
 IMAGES_STORE = '/home/scrapy/data/ctrip_images/'
 
-#DOWNLOAD_HANDLERS = {
-#    'http': 'zqtravel.scrapyjs.dhandler.WebkitDownloadHandler',
-#    'https': 'zqtravel.scrapyjs.dhandler.WebkitDownloadHandler',
-#}
-
-#USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.66 Safari/537.36'
 
 WEBKIT_DOWNLOADER=['mafengwo', 'mafengwo_scenicspot', 'mafengwo_province', 'ctrip', 'ctrip_travel', 'mafengwo_travel', 'chanyouji']
 
@@ -41,7 +34,7 @@ DOWNLOADER_MIDDLEWARES = {
 #    'zqtravel.downloader.WebkitDownloader': 1,
     'scrapy.contrib.downloadermiddleware.useragent.UserAgentMiddleware': None,
     'zqtravel.middleware.rotate_useragent.RotateUserAgentMiddleware': 100,
-    #'zqtravel.scrapyjs.middleware.WebkitDownloader': 1,
+#    'zqtravel.scrapyjs.middleware.WebkitDownloader': 1,
 }
 
 # 启用AutoThrottle扩展
@@ -50,9 +43,7 @@ AUTOTHROTTLE_ENABLED = True
 COOKIES_ENABLED = False
 # 单位是妙。下载器在下载同一个网站下一个页面前需要等待的时间。该选项可以用来限制爬取速度， 减轻服务器压力。同时也支持小数
 #DOWNLOAD_DELAY = 1
-
 # 让爬取的质量更高，使用宽度优先的策略
 #SCHEDULER_ORDER = 'BFO'
-
 # 启用调试
 DUPEFILTER_DEBUG = True
