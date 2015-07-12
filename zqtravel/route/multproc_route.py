35cb277cdcae5979a96eea5f90d555e199db555f
roadsoar <chunchen@redhat.com>
2015-07-12 14:45:44 +0800

    p

--- zqtravel/route/multproc_route.py
+++ zqtravel/route/multproc_route.py
@@ -7,6 +7,7 @@ import requests
 import urllib
 import math
 import traceback, logging
+import multiprocessing
   
 class RouteAnalyzer:
   def __init__(self):
@@ -16,8 +17,8 @@ class RouteAnalyzer:
   def mercator2wgs84(self, mercator):
     point_x = mercator[0]
     point_y = mercator[1]
-    x = point_x / 20037508.3319992 * 180
-    y = point_y / 20037509.3427892 * 180
+    x = point_x / 20037508.3427892 * 180
+    y = point_y / 20037508.3427892 * 180
     y = 180/math.pi*(2*math.atan(math.exp(y*math.pi/180))-math.pi/2)
     return (x, y)
   
@@ -54,54 +55,19 @@ class RouteAnalyzer:
       traceback.print_exc(file=open(log_file,'w'))
       sys.exit(-1)
   
-  def get_location(self, addr):
-    pattern_x = re.compile(r'"lng":(\d+.\d+)')
-    pattern_y = re.compile(r'"lat":(\d+.\d+)')
-    addr_list = addr.split('.')
-    city_name = addr_list[1]
-    province_name = addr_list[0]
-    if len(addr_list) == 3:
-       city_name = addr_list[2]
-       province_name = addr_list[1]
-    api_addr = 'http://api.map.baidu.com/geocoder/v2/?address=%s&output=json&ak=Pk0CeOPzsErMLmnbcY80uGWE&callback=showLocation' % addr.replace('.','')
-    api_addr2 = 'http://api.map.baidu.com/geocoder/v2/?address=%s&city=%s&output=json&ak=Pk0CeOPzsErMLmnbcY80uGWE&callback=showLocation' % (city_name, province_name)
-    api_addr3 = 'http://api.map.baidu.com/geocoder/v2/?address=%s&output=json&ak=Pk0CeOPzsErMLmnbcY80uGWE&callback=showLocation' % (city_name)
-
-    try:
-      resolved_city_file = self.db_cf.get_str('storage','resolved_city_file')
-      req = requests.get(api_addr)
-      content = req.content
-      req2 = requests.get(api_addr2)
-      content2 = req2.content
-      req3 = requests.get(api_addr3)
-      content3 = req3.content
-      x = re.findall(pattern_x,''.join([content, content2, content3]))
-      y = re.findall(pattern_y,''.join([content, content2, content3]))
-      if x:
-        x=x[0]
-        y=y[0]
-        location = (x,y)
-      else:
-        location=()
-      self.write_to_file(resolved_city_file, addr+'\n')
-      return location
-    except Exception as e:
-      log_file = self.db_cf.get_str('logging','log')
-      print "Occur broken errors, please logs (%s)." % (log_file)
-      traceback.print_exc(file=open(log_file,'w'))
-      sys.exit(-1)
-
   def run(self):
     addrs = self.get_unresolved_city()  
-    print "Dealing with longitude and latitude for City/Scenicspot.."
+    print "Dealing with longitude and latitude for Scenicspot.."
     addr_location_file = self.db_cf.get_str('storage','addr_location')
     for addr in addrs:
-        mercator = self.get_location(addr)
-        if not mercator:
-           mercator = ('NotFound','NotFound')
-        location = "%s,%s,%s\n"%(addr,mercator[0],mercator[1])
+        mercator = self.get_mercator(addr)
+        if mercator:
+            wgs = self.mercator2wgs84(mercator)
+        else:
+            wgs=('NotFound','NotFound')
+        location = "%s,%s,%s\n"%(addr,wgs[0],wgs[1])
         self.write_to_file(addr_location_file, location)
-    print "Done! Have %d City/Scenicspots." % len(addrs)
+    print "Done! Have %d Scenicspots." % len(addrs)
 
   def write_to_file(self, file_name, line, f_mode='a+'):
       f = open(file_name, f_mode)
@@ -132,8 +98,7 @@ class RouteAnalyzer:
          f.close()
       if not addr_list:
          print "Generating Scenicspot from database.."
-         sql = "select Province.Province_name, City.City_name from Province, City where City.Province_no = Province.Province_no"
-         #sql = "select Province.Province_name, City.City_name, Scenicspot_name from Province, City, Scenicspot where Scenicspot.City_no = City.City_no and City.Province_no = Province.Province_no"
+         sql = "select Province.Province_name, City.City_name, Scenicspot_name from Province, City, Scenicspot where Scenicspot.City_no = City.City_no and City.Province_no = Province.Province_no"
          data = self.my_db.selectall(sql)
          self.close_mysql()
          for dt in data:
